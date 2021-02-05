91563ddf5b4cff176ea523391c0c60478b4dc94c
Gamer686 <74534832+Gamer686@users.noreply.github.com>
2021-02-05 17:26:17 +0530

    Add files via upload

--- Umbrella.js
+++ Umbrella.js
@@ -5,14 +5,14 @@ class Umbrella{
         }
         this.body = Bodies.circle(x,y,50,options);
         this.radius = 50;
-        this.animation = loadAnimation("walking_1.png","walking_2.png","walking_3.png","walking_4.png","walking_5.png","walking_6.png","walking_7.png","walking_8.png",)
+        this.image = loadImage("walking_1.png")
         
   World.add(world,this.body)
     }
     display(){
         var pos = this.body.position;
-        animationMode(CENTER)
-        animation(this.animation,pos.x,pos.y+70,300,300);
+        imageMode(CENTER)
+        image(this.image,pos.x,pos.y+70,300,300);
     
     }
 }
